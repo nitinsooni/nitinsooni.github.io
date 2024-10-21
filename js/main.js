3e916c4343c3a5a3a03fb701c47fd3c8f98c627f
Nitin Soni <nitinsoooni@gmail.com>
2024-10-21 21:36:02 +0530

    added hamburger and nav-menu overlay

--- js/main.js
+++ js/main.js
@@ -14,6 +14,27 @@ window.addEventListener('load', function() {
     }
 });
 
+// Toggle hamburger activation and nav menu display when user click hamburger
+const hamburger = document.querySelector('nav .nav-l .hamburger');
+const navMenu = document.querySelector('nav .nav-overlay');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+// Disable hamburger and nav menu when user click nav menu's link
+const navLink = document.querySelectorAll('nav .nav-overlay .nav-link');
+
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}
+
 // Show the scroll-to-top button when user scrolls down 20px
 let btnTop = document.querySelector('.scroll-to-top');
 window.onscroll = function() {scrollFunction()};
@@ -31,5 +52,6 @@ function topFunction() {
     document.body.scrollTop = 0; // for safari
     document.documentElement.scrollTop = 0; // for chrome, firefox, ie and opera
 }
+
 // Dynamically change year in copyright text
 document.querySelector('footer .container .wrapper-l p .year').textContent = new Date().getFullYear();
