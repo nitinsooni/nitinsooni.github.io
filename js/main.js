b7f1b83f98a70ded6a4c2136c3dd65bafbb7ad9a
Nitin Soni <nitinsoooni@gmail.com>
2025-05-12 19:58:40 +0530

    disaled the scroll when preloader or nav-overlay are active

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
             preloader.style.display = 'none';
         }, 350);
 
+        document.body.classList.remove('no-scroll-touch');
         locoScroll.start();
 
         const scrollTarget = new URLSearchParams(location.search).get('scrollTo');
@@ -63,6 +64,7 @@ const navMenu = document.querySelector('nav .nav-overlay');
 function mobileMenu() {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+    document.body.classList.toggle('no-scroll-touch');
 }
 
 // Disable hamburger and nav menu when user click nav menu's link
@@ -73,6 +75,7 @@ navLink.forEach((n) => n.addEventListener('click', closeMenu));
 function closeMenu() {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
+    document.body.classList.toggle('no-scroll-touch');
 }
 
 window.location.hostname !== atob('bml0aW5zb29uaS5naXRodWIuaW8=') && (document.body.innerHTML = '');
